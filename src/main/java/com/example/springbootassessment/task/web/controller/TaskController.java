9a534a329bdbc1b42931c7e003a4266a11c91a25
fabian_sellemond <sellemondfabian@gmail.com>
2025-05-19 13:29:53 +0200

    setup

--- src/main/java/com/example/springbootassessment/task/web/controller/TaskController.java
+++ src/main/java/com/example/springbootassessment/task/web/controller/TaskController.java
@@ -4,6 +4,7 @@ import com.example.springbootassessment.task.service.TaskService;
 import com.example.springbootassessment.task.web.dto.TaskDto;
 import com.example.springbootassessment.task.web.mapper.TaskMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
